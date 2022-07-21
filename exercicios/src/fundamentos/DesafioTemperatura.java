5f727de3ca5d6d756666f68196f7920ae6829d99
Cassid Horimi <cassid@outlook.com.br>
2022-07-21 01:15:21 -0300

    Aulas Java

---
+++ exercicios/src/fundamentos/DesafioTemperatura.java
@@ -0,0 +1,13 @@
+package fundamentos;
+
+public class DesafioTemperatura {
+    public static void main(String[] args) {
+        int fahrenheit = 100;
+        final int ADJUSTMENT = 32;
+        final double FACTOR = 5 / 9.0;
+        double celsius = (fahrenheit - ADJUSTMENT) * FACTOR;
+        String result = String.format("%.2f", celsius);
+
+        System.out.println("O resultado é: " + result + "°C.");
+    }
+}
